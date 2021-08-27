4b91eed1b7d439b40ed5938650ed01fc4fbd6dbd
Mlabuschagne <89184337+Mlabuschagne@users.noreply.github.com>
2021-08-27 08:47:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter ID:");
+
+
+            string ID = Console.ReadLine();
+
+            Console.WriteLine("Enter Surname:");
+
+
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Enter username:");
+
+
+            string SurnameName = Console.ReadLine();
+
+            Console.WriteLine("Enter Surname:");
+
+
+            string FirstnameName = Console.ReadLine();
+
+            Console.WriteLine("Enter Gender:");
+
+
+            string Gender = Console.ReadLine();
+
+
+
+
+
+            Console.WriteLine("Username is: " + Name);
+        }
+    }
+}
+
+
+
